f8ca29625e9365a6a5ed4f2d7dbe4a88a093b424
WildanHanif23 <wildan_sulhan_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 13:17:46 +0700

    Add files via upload

--- hello.java
+++ hello.java
@@ -1,6 +1,13 @@
 public class hello {
     public static void main(String[] args) {
-        System.out.println("WildanHanif;");
+        //main method
+        System.out.println("Wildan Hanif Sulhan;");
+        System.out.println("SMK TELKOM MALANG");
+        System.out.println("SMP BUSTANUL MAKMUR");
+        System.out.println("Sulkan");
+        System.out.println("Hori Novita Verni");
+        System.out.println("Berguna Bagi Orang Lain");
+        System.out.println("Nongkrong");
     }   
 }
   

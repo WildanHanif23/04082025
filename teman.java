232b46f20567a0a501de8a25125caae3269f5092
WildanHanif23 <wildan_sulhan_34rpl@student.smktelkom-mlg.sch.id>
2025-08-04 14:25:06 +0700

    Add files via upload

---
+++ teman.java
@@ -0,0 +1,43 @@
+public class teman {
+    public static void main(String[] args) {
+        // data data teman
+        String nama = "Arjuna susila mukti";
+        int usia = 15;
+        String citacita = "progamer";
+        System.out.println(nama);
+        System.out.println(usia);
+        System.out.println(citacita);  
+        
+        String nama2 = "Daffa Aqilah";
+        int usia2 = 15;
+        String citacita2 = "polisi";
+        System.out.println(nama2);
+        System.out.println(usia2);
+        System.out.println(citacita2); 
+        
+        String nama3 = "Farhan Haidas Gasani";
+        int usia3 = 15;
+        String citacita3 = "pengusaha";
+        System.out.println(nama3);
+        System.out.println(usia3);
+        System.out.println(citacita3);
+        
+        String nama4 = "Atha Khoirun Ayyasi";
+        int usia4 = 15;
+        String citacita4 = "pengusaha";
+        System.out.println(nama4);
+        System.out.println(usia4);
+        System.out.println(citacita4); 
+        
+        String nama5= "Erlangga Dwi Firmnsyah";
+        int usia5= 15;
+        String citacita5= "Menjadi orang sukses";
+        System.out.println(nama5);
+        System.out.println(usia5);
+        System.out.println(citacita5);  
+
+
+
+
+    }
+} 
